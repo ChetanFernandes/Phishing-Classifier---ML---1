0de62eff4eea950cc0625480bfd537e0710ff31b
Chetan <chetanfern@gmail.com>
2024-09-25 19:49:29 +0530

    Second commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Phishing-Classifier---ML---1
+pip install -e  - To run setup.py
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Chetan Fernandis",
+    author_email="chetanfern@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
